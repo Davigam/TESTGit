c347e54332db85969ef1fbcfe64479453fa12ab9
Isaac Berdugo <isaacdberdugoa@gmail.com>
2020-08-11 11:23:28 -0500

    first commit

---
+++ src/FunTest.java
@@ -0,0 +1,9 @@
+public class FunTest {
+    final static void TesTrue (){
+        System.out.println("Cause I'm Batman");
+    }
+
+    final static void TestFalse (){
+        System.out.println("I know");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        var TestComplete = true;
+        var TestError = false;
+
+        System.out.println(TestComplete);
+        System.out.println(TestError);
+    }
+}
